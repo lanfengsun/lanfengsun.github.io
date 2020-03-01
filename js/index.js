df1eecc1b3848a809efe1807318e986b5846f4be
Lanfeng Sun <lanfeng.sun@yahoo.ca>
2020-03-01 17:19:22 +0800

    add site code

---
+++ js/index.js
@@ -0,0 +1,7 @@
+// scroll reveal library customization
+const sr = ScrollReveal();
+sr.reveal(".about", { reset: true, easing: "cubic-bezier(.85,.1,.15,.85)" });
+sr.reveal(".experience", {
+  reset: true,
+  easing: "cubic-bezier(.85,.1,.15,.85)"
+});
